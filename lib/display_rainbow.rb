bd5fb2d33957935f102c6317067cddd32086143a
Jacquelyn Hardrick <jhardrick68@outlook.com>
2019-01-09 15:52:48 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,3 +1,3 @@
 def display_rainbow(colors)
-  puts "#{R: red[0]}", "#{O: orange[1]}", "#{Y: yellow[2]}" "#{G: green[3]}", "#{B: blue[4]}", "#{I: indigo[5]}", "#{V: violet[6]}"
+  puts "#{colors[0]}", "#{colors[1]}", "#{colors[2]}" "#{colors[3]}", "#{colors[4]}", "#{colors[5]}", "#{colors[6]}"
 end
